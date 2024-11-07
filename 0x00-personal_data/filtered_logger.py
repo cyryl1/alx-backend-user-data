1368079c99a0ae1ab23f185e9e74ba3fa6cbe79f
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-07 19:37:38 +0100

    Edit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,9 +15,8 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Returns the log message obfuscate"""
     for f in fields:
-        message = re.sub(f"{f}=.*?{separator}",
-                         f"{f}={redaction}{separator}", message)
-    return message
+        return re.sub(f"{f}=.*?{separator}",
+                      f"{f}={redaction}{separator}", message)
 
 
 def get_logger() -> logging.Logger:
@@ -89,7 +88,6 @@ class RedactingFormatter(logging.Formatter):
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
-
 
     if __name__ == '__main__':
         main()
