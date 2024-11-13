b6959f2514c1268fb385b660ec7a23a76b501b13
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-13 13:01:00 +0100

    Commit

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
+"""Authentication"""
 
 from flask import request
 from typing import List, TypeVar
-"""Authentication"""
 
 
 class Auth:
+    """
+    Authentication class template
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks if the request path is authenticated"""
         return False
-    
 
     def authorization_header(self, request=None) -> str:
         """Returns the authorization header from the request"""
         return None
-    
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
-        Returns the current authenticated user based on the authorization header
+        Returns the current authenticated user based
+        on the authorization header
         """
         return None
