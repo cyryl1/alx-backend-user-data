f9ee8ddab74ca63fd8ab66c3d144fcc0a259e540
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-13 15:09:15 +0100

    commit

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """Basic Authentication"""
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,19 @@ class BasicAuth(Auth):
             return None
         base64_encoded_credentials = authorization_header[6:]
         return base64_encoded_credentials
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """Decodes base64 credentials"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_credentials = base64.b64decode(
+                base64_authorization_header
+                ).decode('utf-8')
+            return decoded_credentials
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
