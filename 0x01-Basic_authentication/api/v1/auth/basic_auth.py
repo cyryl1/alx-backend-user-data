f25439d4bf47e115fb919a5a87ea9bdf6e9dcc3a
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-13 14:58:46 +0100

    commit

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,14 @@ class BasicAuth(Auth):
     """
     Class Representation of Basic Authentication
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extracts the credentials needed for authentication"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        base64_encoded_credentials = authorization_header[6:]
+        return base64_encoded_credentials
