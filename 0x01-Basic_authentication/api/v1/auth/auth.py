e338b905304879aa6f2c3d748bb4d18825d3dbc4
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-13 13:30:53 +0100

    Commit

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,20 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks if the request path is authenticated"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        for excluded_path in excluded_paths:
+            if not excluded_path.endswith('/'):
+                excluded_paths.append(excluded_path + '/')
+
+        norm_path = path if path.endswith('/') else path + '/'
+        for excluded_path in excluded_paths:
+            if norm_path.startswith(excluded_path):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns the authorization header from the request"""
