a364cda3a0a74efe4dd382934f02ab8c77134fe1
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-14 16:10:55 +0100

    Task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -12,6 +12,9 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """
+        Create a session id
+        """
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
