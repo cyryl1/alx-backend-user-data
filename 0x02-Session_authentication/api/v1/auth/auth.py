d1cd7c4867404dd9e7cd4e11ad31188aeb87bb3b
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-14 16:35:59 +0100

    Task 4

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 from flask import request
 from typing import List, TypeVar
 import fnmatch
+import os
 
 
 class Auth:
@@ -47,9 +48,12 @@ class Auth:
         on the authorization header
         """
         return None
-    
+
     def session_cookie(self, request=None):
         """Session cookie"""
         if request is None:
             return None
-        
+
+        session_name = os.getenv("SESSION_NAME", "_my_session_id")
+
+        return request.cookies.get(session_name)
