cbe7d9a140b803b3dab9ec04bda20e6322fbdad9
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-19 16:23:25 +0100

    Commit

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 import uuid
+from typing import Optional
 
 
 def _hash_password(password: str) -> bytes:
@@ -71,3 +72,19 @@ class Auth:
         session_id = _generate_uuid()
         user.session_id = session_id
         return session_id
+
+    def get_user_from_session_id(self, session_id: Optional[str]) -> Optional[User]:
+        """
+        Finds a user using the session_id
+        Args:
+            session_id: str
+        Returns:
+            user object.
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
