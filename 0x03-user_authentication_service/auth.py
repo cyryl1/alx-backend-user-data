d9810ccde58e98c6b52173e8f7b68a4e681ad221
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-19 14:59:21 +0100

    Tas 9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,7 +14,7 @@ def _hash_password(password: str) -> bytes:
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     """
     Generate uuid
     Returns:
