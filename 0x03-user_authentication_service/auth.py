a37293aa8bc1a05d8771e7be7697da6ce0786c6f
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-19 14:53:44 +0100

    Task 9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -47,3 +48,11 @@ class Auth:
             return True
         else:
             return False
+
+    def _generate_uuid(self) -> uuid:
+        """
+        Generate uuid
+        Returns:
+            a string representation of a new UUID
+        """
+        return str(uuid.uuid4())
