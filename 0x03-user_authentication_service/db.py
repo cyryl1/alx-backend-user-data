0ec5dd9f34e34520e8836aec4680394f9081538f
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-19 14:08:07 +0100

    Commit

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,7 +34,14 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """
-        Add a new user to the database and return the user object
+        Add a new user to the database
+
+        Args:
+            email (str): The email of the user.
+            hashed_password (str): The hashed password of the user.
+
+        Returns:
+            User: The added user object.
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
