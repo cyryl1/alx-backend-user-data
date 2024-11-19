55ee86ab0f4a7da9ba5e87a53f89d51ffe20bca6
Praise Aribisala <aribisalapraise12@gmail.com>
2024-11-19 17:33:41 +0100

    Commit

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -95,5 +95,17 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    email = request.form.get('email')
+    reset_token = AUTH.get_reset_password_token(email=email)
+    if not reset_token:
+        abort(403)
+    return jsonify({
+        "email": email,
+        "reset_token": reset_token
+    }), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
